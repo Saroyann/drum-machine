2bcd21465f8ea0f54cfe14a2c762e3a8d15c7833
Yiim <ilyimyan@gmail.com>
2024-08-19 09:41:54 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <>
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-[700px] h-[400px] bg-slate-700"></div>
+    </div>
+    </>
+  )
+}
+
+export default App
